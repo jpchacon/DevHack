e9a1e90c164e150353f6dca95f079b205c4af663
jpchacon <j-pablo-cp@hotmail.com>
2017-12-16 11:00:53 -0500

    ajustes

--- DevHack/app/src/main/java/co/devhack/presentation/view/fragments/ListWork.java
+++ DevHack/app/src/main/java/co/devhack/presentation/view/fragments/ListWork.java
@@ -10,17 +10,21 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import co.devhack.R;
+import co.devhack.presentation.interfaces.ListWorkContract;
+import co.devhack.presentation.presenters.ListWorkPresenter;
+import co.devhack.presentation.view.adapters.WorkAdapter;
 
 /**
  * A simple {@link Fragment} subclass.
  */
-public class ListWork extends Fragment {
+public class ListWork extends Fragment implements ListWorkContract.View{
+
+    private ListWorkContract.UserActionsListener mActionsListener;
 
     private RecyclerView recyclerView;
 
 
     public ListWork() {
-        // Required empty public constructor
     }
 
     public static ListWork getInstance(){
@@ -33,14 +37,24 @@ public class ListWork extends Fragment {
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_list_work, container, false);
 
+        mActionsListener = new ListWorkPresenter(this);
+
         recyclerView = view.findViewById(R.id.rvWorkList);
 
         LinearLayoutManager layoutManager = new LinearLayoutManager(getContext());
         layoutManager.setOrientation(LinearLayoutManager.VERTICAL);
         recyclerView.setLayoutManager(layoutManager);
 
+        WorkAdapter workAdapter = new WorkAdapter(mActionsListener.getLstWorks());
+        recyclerView.setAdapter(workAdapter);
+
+        mActionsListener.loadAll();
 
         return view;
     }
 
+    @Override
+    public void refreshTodos() {
+        recyclerView.getAdapter().notifyDataSetChanged();
+    }
 }
