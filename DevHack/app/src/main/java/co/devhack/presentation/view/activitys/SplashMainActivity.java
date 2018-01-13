fe485491c648bbdeb311bad17d9a5da2759f78f6
jpchacon <j-pablo-cp@hotmail.com>
2018-01-12 23:15:16 -0500

    Aplicación DavHack versión 1.2

---
+++ DevHack/app/src/main/java/co/devhack/presentation/view/activitys/SplashMainActivity.java
@@ -0,0 +1,22 @@
+package co.devhack.presentation.view.activitys;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Juanpa on 12/1/2018.
+ */
+
+public class SplashMainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        Intent intent = new Intent(this,MainActivity.class);
+        startActivity(intent);
+        finish();
+    }
+}
